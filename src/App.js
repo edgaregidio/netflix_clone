7e46dc3b13bd0cd8ad5d7d35420f4f07793a9fdb
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2021-11-12 20:31:58 -0300

    header and footer completed.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Tmdb from "./Tmdb";
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -25,15 +27,36 @@ export default function App() {
     };
     loadAll();
   }, []);
+
+  useEffect(() => {
+    const scrowListener = () => {
+      if (window.scrollY > 10) {
+        setBlackHeader(true);
+      } else {
+        setBlackHeader(false);
+      }
+    };
+    window.addEventListener("scroll", scrowListener);
+    return () => {
+      window.removeEventListener("scroll", scrowListener);
+    };
+  }, []);
   console.log(movieList);
   return (
     <div className="page">
+      <Header black={blackHeader} />
       {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
         ))}
       </section>
+      <footer>
+        Desenvolvido apenas para fins estudantis por Edgaregidio
+        <br />
+        Todos os direitos de imgagem para Netflix e dados pegos do site
+        Themoviedb.org .
+      </footer>
     </div>
   );
 }
