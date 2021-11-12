5fb5b62c75c3b54326c13c30d484112ebd2782a0
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2021-11-12 05:04:44 -0300

    Completed movie list api.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Tmdb from "./Tmdb";
+import MovieRow from "./components/MovieRow";
+import FeaturedMovie from "./components/FeaturedMovie";
 
 export default function App() {
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // PEGANDO A LISTA TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    };
+    loadAll();
+  }, []);
+  console.log(movieList);
   return (
-    <div>
-      <h1>teste as</h1>
+    <div className="page">
+      <FeaturedMovie />
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   );
 }
