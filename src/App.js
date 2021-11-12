adc0487e51a304b051396035c58bc01ffeba1449
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2021-11-12 05:31:20 -0300

    presentation settings configured with API

--- src/App.js
+++ src/App.js
@@ -6,20 +6,29 @@ import FeaturedMovie from "./components/FeaturedMovie";
 
 export default function App() {
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
   useEffect(() => {
     const loadAll = async () => {
       // PEGANDO A LISTA TOTAL
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+
+      //PEGANDO O FEATURED
+      let originals = list.filter((i) => i.slug === "originals");
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      );
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+      setFeaturedData(chosenInfo);
     };
     loadAll();
   }, []);
   console.log(movieList);
   return (
     <div className="page">
-      <FeaturedMovie />
-
+      {featuredData && <FeaturedMovie item={featuredData} />}
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items} />
