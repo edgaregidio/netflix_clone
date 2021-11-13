f7c9ab60730a58e81f9dc447b9b6c757470d4ae5
Edgar Egidio <37923860+edgaregidio@users.noreply.github.com>
2021-11-13 06:22:11 -0300

    netflix clone page finished

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -8,6 +8,11 @@ export default function FeaturedMovie({ item }) {
     genres.push(item.genres[i].name);
   }
 
+  let description = item.overview;
+  if (description.length > 200) {
+    description = description.substring(0, 200) + "...";
+  }
+
   return (
     <section
       className="featured"
@@ -28,7 +33,7 @@ export default function FeaturedMovie({ item }) {
               {item.number_of_seasons !== 1 ? "s" : ""}
             </h2>
           </div>
-          <p>{item.overview}</p>
+          <p>{description}</p>
           <div className="featured--buttons">
             <a className="featured--watchbutton" href={`/watch/${item.id}`}>
               Assistir
